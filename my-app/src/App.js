b6dc09d2092f7272052c09fdb6419af2cedba272
Gowtham M <gowtham.1392@gmail.com>
2019-07-26 20:55:40 +0530

    Count increment

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,12 +3,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import Greet from "./components/Greet";
 import Message from "./components/Message";
+import Count from "./components/Count";
 
 function App() {
   return (
     <div className="App">
-      <Greet name="Gowtham" id="1" />
-      <Message />
+      {/* <Greet name="Gowtham" id="1" />
+      <Message /> */}
+      <Count />
     </div>
   );
 }
---
+++ my-app/src/components/Count.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+class Count extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0
+    };
+  }
+  handleIncrement() {
+    this.setState(prevState => ({
+      count: prevState.count + 1
+    }));
+  }
+  render() {
+    return (
+      <div>
+        <h1>Count = {this.state.count}</h1>
+        <button onClick={() => this.handleIncrement()}>Increment</button>
+      </div>
+    );
+  }
+}
+
+export default Count;
